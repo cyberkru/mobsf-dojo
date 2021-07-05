e6ee31e97b0c8b5648263f76571a4fc936cce9c1
cyberkru <junadi3000@gmail.com>
2021-07-05 10:35:39 +0700

    update scan.sh

--- scan/scan.sh
+++ scan/scan.sh
@@ -11,8 +11,8 @@ FILE_NAME=`echo $response | jq '.file_name' | sed 's/"//g'`
 HASH=`echo $response | jq '.hash' | sed 's/"//g'`
 
 scan=`curl -X POST --url "${MOBSF_URL}api/v1/scan" --data "scan_type=$SCAN_TYPE&file_name=$FILE_NAME&hash=$HASH" -H "Authorization:12345"`
-curl -X POST --url "${MOBSF_URL}api/v1/report_json" --data "hash=$HASH" -H "Authorization:12345" > output/report.json
-curl -X POST --url "${MOBSF_URL}api/v1/download_pdf" --data "hash=$HASH" -H "Authorization:12345" > output/report.pdf
+json=`curl -X POST --url "${MOBSF_URL}api/v1/report_json" --data "hash=$HASH" -H "Authorization:12345" > output/report.json`
+pdf=`curl -X POST --url "${MOBSF_URL}api/v1/download_pdf" --data "hash=$HASH" -H "Authorization:12345" > output/report.pdf`
 
 
 curl -X POST --header "Content-Type:multipart/form-data" --header "Authorization:Token $DOJOKEY" -F "engagement=${EGID}" -F "scan_type=MobSF Scan" -F 'file=@./output/report.json' --url "http://${DOJOIP}/api/v2/import-scan/"
