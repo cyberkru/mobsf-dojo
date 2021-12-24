f929cf469ec4a7240d5c4d2e0fc85b527b0f0536
cyberkru <74045103+cyberkru@users.noreply.github.com>
2021-12-24 13:57:09 +0700

    Update scan.sh

--- scan/scan.sh
+++ scan/scan.sh
@@ -13,9 +13,9 @@ FILE_NAME=`echo $response | jq '.file_name' | sed 's/"//g'`
 HASH=`echo $response | jq '.hash' | sed 's/"//g'`
 
 scan=`curl -X POST --url "${MOBSF_URL}api/v1/scan" --data "scan_type=$SCAN_TYPE&file_name=$FILE_NAME&hash=$HASH" -H "Authorization:12345"`
-json=`curl -X POST --url "${MOBSF_URL}api/v1/report_json" --data "hash=$HASH" -H "Authorization:12345" > report.json`
-pdf=`curl -X POST --url "${MOBSF_URL}api/v1/download_pdf" --data "hash=$HASH" -H "Authorization:12345" > report.pdf`
+json=`curl -X POST --url "${MOBSF_URL}api/v1/report_json" --data "hash=$HASH" -H "Authorization:12345" > output/report.json`
+pdf=`curl -X POST --url "${MOBSF_URL}api/v1/download_pdf" --data "hash=$HASH" -H "Authorization:12345" > output/report.pdf`
 
 PRODID=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token ${DOJOKEY}" --url "http://{$DOJOIP}/api/v2/products/?limit=1000" | jq -c '[.results[] | select(.name | contains('\"${PRODNAME}\"'))][0] | .id')
 EGID=$(curl -s -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Token $DOJOKEY" --url "http://${DOJOIP}/api/v2/engagements/?limit=1000" | jq -c "[.results[] | select(.product == ${PRODID})][0] | .id")
-curl -X POST --header "Content-Type:multipart/form-data" --header "Authorization:Token $DOJOKEY" -F "engagement=${EGID}" -F "scan_type=MobSF Scan" -F 'file=@./report.json' --url "http://${DOJOIP}/api/v2/import-scan/"
+curl -X POST --header "Content-Type:multipart/form-data" --header "Authorization:Token $DOJOKEY" -F "engagement=${EGID}" -F "scan_type=MobSF Scan" -F 'file=@./output/report.json' --url "http://${DOJOIP}/api/v2/import-scan/"
